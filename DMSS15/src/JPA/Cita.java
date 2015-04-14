9d1c70a66dcfd48555f12dce838b1b0339679f1e
Juan Carlos Alcaide <Juan Carlos@algojuca-PC>
2015-04-14 15:25:41 +0200

    Cita terminada, a falta de relaciones

--- DMSS15/src/JPA/Cita.java
+++ DMSS15/src/JPA/Cita.java
@@ -6,10 +6,13 @@
 package JPA;
 
 import java.io.Serializable;
+import java.util.Date;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
 
 /**
  *
@@ -20,39 +23,48 @@ public class Cita implements Serializable {
     private static final long serialVersionUID = 1L;
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
-    private Long id;
+    private Long codigo;
+    
+    @Temporal(TemporalType.DATE)
+    private Date fecha;
+    private String hora;
+    private Integer duracion;
+    
+    //Faltan relaciones centro y municipio
 
-    public Long getId() {
-        return id;
+    public Long getCodigo() {
+        return codigo;
     }
 
-    public void setId(Long id) {
-        this.id = id;
+    public void setCodigo(Long codigo) {
+        this.codigo = codigo;
     }
 
-    @Override
-    public int hashCode() {
-        int hash = 0;
-        hash += (id != null ? id.hashCode() : 0);
-        return hash;
+    public Date getFecha() {
+        return fecha;
     }
 
-    @Override
-    public boolean equals(Object object) {
-        // TODO: Warning - this method won't work in the case the id fields are not set
-        if (!(object instanceof Cita)) {
-            return false;
-        }
-        Cita other = (Cita) object;
-        if ((this.id == null && other.id != null) || (this.id != null && !this.id.equals(other.id))) {
-            return false;
-        }
-        return true;
+    public void setFecha(Date fecha) {
+        this.fecha = fecha;
     }
 
-    @Override
-    public String toString() {
-        return "JPA.Cita[ id=" + id + " ]";
+    public String getHora() {
+        return hora;
+    }
+
+    public void setHora(String hora) {
+        this.hora = hora;
+    }
+
+    public Integer getDuracion() {
+        return duracion;
+    }
+
+    public void setDuracion(Integer duracion) {
+        this.duracion = duracion;
     }
     
+    
+    
+
 }
