ef1f4aaf338dc7e75395f90149c8e451259fd022
Rubencito <Rubencito@RUBEN>
2015-04-14 16:56:46 +0200

    Ruben

---
+++ DMSS15/src/JPA/Recurso.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package JPA;
+
+import javax.persistence.Entity;
+
+/**
+ *
+ * @author Rubencito
+ */
+
+@Entity
+
+public class Recurso {
+    
+}
