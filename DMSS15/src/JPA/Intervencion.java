b3460db9fb2145d763be28de83c6bb932069cf9a
Salvador <Salvador@SUNNY>
2015-04-15 14:23:26 +0200

    Eliminadas librerías no usadas en Intervención

--- DMSS15/src/JPA/Intervencion.java
+++ DMSS15/src/JPA/Intervencion.java
@@ -11,8 +11,6 @@ import java.util.List;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.JoinColumn;
-import javax.persistence.JoinTable;
 import javax.persistence.ManyToMany;
 
 /**
